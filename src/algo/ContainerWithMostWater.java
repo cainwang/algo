e45858946bddb481015b23812c92b0aa623fe652
Cain Wang <cainwang@vmware.com>
2014-08-13 10:49:04 -0700

    Mark 48

---
+++ src/algo/ContainerWithMostWater.java
@@ -0,0 +1,45 @@
+/**
+ *
+ */
+package algo;
+
+/**
+ * @author cainwang
+ *
+ */
+public class ContainerWithMostWater {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        ContainerWithMostWater solution = new ContainerWithMostWater();
+        System.out.println(solution.maxArea(new int[] { 1, 2, 3 }));
+    }
+
+    public int maxArea(int[] heights) {
+        int maxArea = 0;
+        int start = 0, end = heights.length - 1;
+
+        while (start < end) {
+            int startHeight = heights[start], endHeight = heights[end];
+
+            int area = Math.min(startHeight, endHeight) * (end - start);
+            if (area > maxArea) {
+                maxArea = area;
+            }
+
+            if (startHeight < endHeight) {
+                while (start < end && heights[start] <= startHeight) {
+                    start ++;
+                }
+            } else {
+                while (start < end && heights[end] <= endHeight) {
+                    end --;
+                }
+            }
+        }
+
+        return maxArea;
+    }
+}
---
+++ src/algo/SpiralMatrix2.java
@@ -0,0 +1,75 @@
+/**
+ *
+ */
+package algo;
+
+import java.util.Arrays;
+
+
+/**
+ * @author cainwang
+ *
+ */
+public class SpiralMatrix2 {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpiralMatrix2 solution = new SpiralMatrix2();
+        int[][] matrix = solution.generateMatrix(2);
+        System.out.println(Arrays.toString(matrix));
+    }
+
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+
+        String direction = "right";
+        int row = 0, col = 0;
+
+        for (int i = 1; i <= n * n; i++) {
+            matrix[row][col] = i;
+
+            switch (direction) {
+            case "up":
+                if (row == 0 || matrix[row - 1][col] > 0) {
+                    direction = "right";
+                    col ++;
+                } else {
+                    row--;
+                }
+
+                break;
+            case "down":
+                if (row == n - 1 || matrix[row + 1][col] > 0) {
+                    direction = "left";
+                    col --;
+                } else {
+                    row++;
+                }
+
+                break;
+            case "left":
+                if (col == 0 || matrix[row][col - 1] > 0) {
+                    direction = "up";
+                    row --;
+                } else {
+                    col--;
+                }
+
+                break;
+            case "right":
+                if (col == n - 1 || matrix[row][col + 1] > 0) {
+                    direction = "down";
+                    row ++;
+                } else {
+                    col++;
+                }
+
+                break;
+            }
+        }
+
+        return matrix;
+    }
+}
