5ee077c46b1d13f02b19121d80aaff97b4bae95a
Cain Wang <cainwang@vmware.com>
2014-08-18 18:01:41 -0700

    mark 64 -

---
+++ src/algo/LongestConsecutiveSequence.java
@@ -0,0 +1,80 @@
+/**
+ *
+ */
+package algo;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author cainwang
+ *
+ */
+public class LongestConsecutiveSequence {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[] source = { 7, -9, 3, -6, 3, 5, 3, 6, -2, -5, 8, 6, -4, -6, -4, -4, 5, -9, 2, 7, 0, 0 };
+
+        LongestConsecutiveSequence solution = new LongestConsecutiveSequence();
+        int result = solution.longestConsecutive(source);
+
+        System.out.println(result);
+    }
+
+    public int longestConsecutive(int[] nums) {
+        Map<Integer, List<Integer>> headMap = new HashMap<>();
+        Map<Integer, List<Integer>> tailMap = new HashMap<>();
+
+        for (int num : nums) {
+            int before = num - 1;
+            int after = num + 1;
+
+            List<Integer> afterList = headMap.get(after);
+            List<Integer> beforeList = tailMap.get(before);
+
+            if (afterList == null && beforeList == null) {
+                List<Integer> list = new LinkedList<>();
+                list.add(num);
+
+                if (headMap.get(num) == null && tailMap.get(num) == null) {
+                    headMap.put(num, list);
+                    tailMap.put(num, list);
+                }
+            } else if (afterList != null && beforeList != null) {
+                beforeList.add(num);
+                beforeList.addAll(afterList);
+
+                headMap.remove(after);
+                tailMap.remove(before);
+                tailMap.put(beforeList.get(beforeList.size() - 1), beforeList);
+            } else if (afterList != null) {
+                afterList.add(0, num);
+                headMap.remove(after);
+                headMap.put(num, afterList);
+            } else if (beforeList != null) {
+                beforeList.add(num);
+                tailMap.remove(before);
+                tailMap.put(num, beforeList);
+            }
+
+        }
+
+        int maxLen = 0;
+
+        for (Map.Entry<Integer, List<Integer>> entry : headMap.entrySet()) {
+            List<Integer> list = entry.getValue();
+
+            if (list.size() > maxLen) {
+                maxLen = list.size();
+            }
+        }
+
+        return maxLen;
+    }
+
+}
